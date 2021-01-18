a5f6f4811d12dd9555a3fa0260c5de3a9c4caef0
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-18 16:22:56 -0500

    projects.json

---
+++ src/components/Project/Project.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function Projects() {
+    return (
+        <h1>Project 1</h1>
+    )
+}
+
+export default Projects;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Project from "../components/Project/Project";
 import "./style.css";
 
 function Portfolio() {
+
     return (
         <div className="container">
             <h2 className="m-3">Portfolio</h2>
+            <div className="container">
+                projects.map(project => {
+                    <Project 
+                    title=project.title/>
+                }
+            </div>
         </div>
     )
 }
